9c627c7745b344f1ca909a2330d214ae20a98832
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-23 16:58:26 +0300

    //

--- SecondDjangoProject/settings.py
+++ SecondDjangoProject/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig',
-    'onetoone.apps.OnetooneConfig'
+    'posts.apps.PostsConfig'
 ]
 
 MIDDLEWARE = [
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from posts.models import Post
 
-# Create your views here.
+
+def index(request):
+    queried = Post.objects.all()
+    print(queried)
+    return render(request, 'index.html', context={'posts':queried})
